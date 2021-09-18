7cc0c102eba7a92586e4710bc3844c6a0fce6dc8
Vinitpal <vs361017@gmail.com>
2021-09-18 10:21:16 +0530

    fixed typos

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json()); // req.body
+app.use(express.static("client/build"));
 
 if (process.env.NODE_ENV === "production") {
   // server static content
